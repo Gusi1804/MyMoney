26736857ac2a9219fb1974f7c7bc606c0101cf64
Gustavo Garfias <gustavo.egarfias@gmail.com>
2022-10-11 20:54:23 -0500

    Added signup function

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-//var admin = require("firebase-admin"); // Import firebase-admin module, used to connect to Firebase Backend for Firebase Realtime Database
-
-//var functions = require('./functions'); // Import custom functions
-
-//var db = admin.database(); // Start Firebase Realtime Database object
-
 router.get('/', (req, res) => {
     res.render('login', { title: "Login" });
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup', { title: "Signup" });
 });
 
 module.exports = router;
